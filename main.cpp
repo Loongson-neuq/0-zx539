592fe4dde1e73488647f7f0ad898591bd7e8df15
zx539 <2198762717@qq.com>
2024-10-08 17:24:54 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,7 +1,14 @@
-#include ".assignment/test.h"
-
-// 比较三个数的大小
+#include ".assignment/test.h"           // 比较三个数的大小
+struct Result {
+    int min;
+    int mid;
+    int max;
+};
 void CompareThreeNumbers(int a, int b, int c, Result &result)
 {
-    // 在这里完成你的代码
+    cin >> a >> b >> c;
+    result.min =
+    result.mid =
+    result.max =
+                                               // 在这里完成你的代码
 }
